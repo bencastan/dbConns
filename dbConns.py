eb74d4a5d53b8db67d9e508fe475039b62d88cd8
Ben Castan <ben@castanfamily.com>
2016-08-15 21:18:37 +1000

    Added correct syntax for delete command , have to work out how to get it into the script

--- dbConns.py
+++ dbConns.py
@@ -9,36 +9,26 @@ db_host = 'db-01.sitesuite.net'
 database = 'ss'
 db_user = 'ss'
 db_password = ''
-interval = "'5'"
+interval = "'5 minutes'"
 max_limit = 200
 
 con = None
 
 try:
 
-    con = psycopg2.connect(host=db_host, database=database, user=db_user, password = 'maid3fee')
+    con = psycopg2.connect(host=db_host, database=database, user=db_user, password = db_password)
     cur = con.cursor()
     #cur.execute('SELECT version()')
     try:
         cur.execute("SELECT count(*) from pg_stat_activity where usename = 'ss' and state = 'idle' and query_start < current_timestamp - interval '5 minutes';")
     except:
-        print "I cant SELECT from bar"
+        print "I can't SELECT from pg_stat_activity"
 
     rows = cur.fetchall()
     print "\nRows: \n"
     for row in rows:
         print row[0]
 
-
-    #ver = cur.fetchone()
-    #print ver
-
-
-    #rows = cur.fetchall()
-    #strRows = str(rows)
-    #print len(strRows)
-    #for row in rows:
-    #    print "   ", row[1][1]
 
 except psycopg2.DatabaseError, e:
     print 'Error %s' % e
@@ -51,8 +41,17 @@ finally:
         con.close()
 
 
-query = "select count(*) from pg_stat_activity where usename = 'ss' and state = 'idle'and query_start < current_timestamp - interval interval;"
 
-if query > max_limit:{
 
-}
+
+#query = "select count(*) from pg_stat_activity where usename = 'ss' and state = 'idle'and query_start < current_timestamp - interval interval;"
+
+#TODO add this query in to run if the number of idel DB connections gets above a limit, probably around 1500 to 200
+#delete_query = " select pg_terminate_backend(pid)
+#from pg_stat_activity
+#where usename = 'ss'
+#and state = 'idle'
+#and query_start < current_timestamp - interval '120 minutes'; "
+#if query > max_limit:
+
+
