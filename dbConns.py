9ef2c1ebc07125b26fc1b0258f0ccc671b114ff0
Ben Castan <ben@castanfamily.com>
2016-08-15 22:06:26 +1000

    Added some colouring to the output and we can now get output that can alert to issues.

--- dbConns.py
+++ dbConns.py
@@ -7,45 +7,57 @@
 
 import psycopg2
 import sys
+import colorama
+from colorama import Fore
 
 db_host = 'db-01.sitesuite.net'
 database = 'ss'
 db_user = 'ss'
 db_password = ''
 interval = "'5 minutes'"
-max_limit = 200
+countMin = 5
+countMid= 20
+countMax = 30
 
 con = None
 
-try:
-
-    con = psycopg2.connect(host=db_host, database=database, user=db_user, password = db_password)
-    cur = con.cursor()
-    #cur.execute('SELECT version()')
+def dbConnect():
+    con = None
     try:
-        cur.execute("SELECT count(*) from pg_stat_activity where usename = 'ss' and state = 'idle' and query_start < current_timestamp - interval '5 minutes';")
-    except:
-        print "I can't SELECT from pg_stat_activity"
 
-    rows = cur.fetchall()
-    print "\nRows: \n"
-    for row in rows:
-        print row[0]
+        con = psycopg2.connect(host=db_host, database=database, user=db_user, password = db_password)
+        cur = con.cursor()
+        #cur.execute('SELECT version()')
+        try:
+            cur.execute("SELECT count(*) from pg_stat_activity where usename = 'ss' and state = 'idle' and query_start < current_timestamp - interval '60 minutes';")
+        except:
+            print "I can't SELECT from pg_stat_activity"
 
-
-except psycopg2.DatabaseError, e:
-    print 'Error %s' % e
-    sys.exit(1)
-
-
-finally:
-
-    if con:
-        con.close()
+        rows = cur.fetchall()
+        #print "\nRows: \n"
+        for row in rows:
+            #print row[0]
+            return row[0]
 
 
+    except psycopg2.DatabaseError, e:
+        print 'Error %s' % e
+        sys.exit(1)
 
 
+    finally:
+        if con:
+            con.close()
+
+
+
+rowCount = dbConnect()
+if rowCount < countMin :
+    print (Fore.GREEN + "Idle queries: " + str(rowCount))
+elif rowCount >= countMid and rowCount <=countMax:
+    print(Fore.YELLOW + "Idle queries: " + str(rowCount))
+elif rowCount >= countMax:
+    print (Fore.RED + "Idle queries: " + str(rowCount))
 
 #query = "select count(*) from pg_stat_activity where usename = 'ss' and state = 'idle'and query_start < current_timestamp - interval interval;"
 
